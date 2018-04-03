c1b964f58cc1c809af47a9416d76317817625597
Akash Agarwal <akash.kumar@axisrooms.com>
2018-04-03 23:39:25 +0530

    default

--- config/env/default.js
+++ config/env/default.js
@@ -1,122 +1,77 @@
 'use strict';
 
-var defaultEnvConfig = require('./default');
-
 module.exports = {
-  db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGODB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/union',
-    options: {},
-    // Enable mongoose debug mode
-    debug: process.env.MONGODB_DEBUG || false
+  app: {
+    title: 'Union Roadways',
+    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
+    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
-  log: {
-    // logging with Morgan - https://github.com/expressjs/morgan
-    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-    format: 'dev',
-    fileLogger: {
-      directoryPath: process.cwd(),
-      fileName: 'app.log',
-      maxsize: 10485760,
-      maxFiles: 2,
-      json: false
+  db: {
+    promise: global.Promise
+  },
+  port: process.env.PORT || 3000,
+  host: process.env.HOST || '0.0.0.0',
+  // DOMAIN config should be set to the fully qualified application accessible
+  // URL. For example: https://www.myapp.com (including port if required).
+  domain: process.env.DOMAIN,
+  // Session Cookie settings
+  sessionCookie: {
+    // session expiration is set by default to 24 hours
+    maxAge: 24 * (60 * 60 * 1000),
+    // httpOnly flag makes sure the cookie is only accessed
+    // through the HTTP protocol and not JS/browser
+    httpOnly: true,
+    // secure cookie should be turned to true to provide additional
+    // layer of security so that the cookie is set only when working
+    // in HTTPS mode.
+    secure: false
+  },
+  // sessionSecret should be changed for security measures and concerns
+  sessionSecret: process.env.SESSION_SECRET || 'MEAN',
+  // sessionKey is the cookie session name
+  sessionKey: 'sessionId',
+  sessionCollection: 'sessions',
+  // Lusca config
+  csrf: {
+    csrf: false,
+    csp: false,
+    xframe: 'SAMEORIGIN',
+    p3p: 'ABCDEF',
+    xssProtection: true
+  },
+  logo: 'modules/core/client/img/brand/logo.png',
+  favicon: 'modules/core/client/img/brand/favicon.ico',
+  illegalUsernames: ['meanjs', 'administrator', 'password', 'admin', 'user',
+    'unknown', 'anonymous', 'null', 'undefined', 'api'
+  ],
+  aws: {
+    s3: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+      bucket: process.env.S3_BUCKET
     }
   },
-  app: {
-    title: defaultEnvConfig.app.title + ' - Development Environment'
-  },
-  facebook: {
-    clientID: process.env.FACEBOOK_ID || 'APP_ID',
-    clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/facebook/callback'
-  },
-  twitter: {
-    username: '@TWITTER_USERNAME',
-    clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-    clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-    callbackURL: '/api/auth/twitter/callback'
-  },
-  google: {
-    clientID: process.env.GOOGLE_ID || 'APP_ID',
-    clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/google/callback'
-  },
-  linkedin: {
-    clientID: process.env.LINKEDIN_ID || 'APP_ID',
-    clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/linkedin/callback'
-  },
-  github: {
-    clientID: process.env.GITHUB_ID || 'APP_ID',
-    clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
-    callbackURL: '/api/auth/github/callback'
-  },
-  paypal: {
-    clientID: process.env.PAYPAL_ID || 'CLIENT_ID',
-    clientSecret: process.env.PAYPAL_SECRET || 'CLIENT_SECRET',
-    callbackURL: '/api/auth/paypal/callback',
-    sandbox: true
-  },
-  mailer: {
-    from: process.env.MAILER_FROM || 'MAILER_FROM',
-    options: {
-      service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
-      auth: {
-        user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
-        pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+  uploads: {
+    // Storage can be 'local' or 's3'
+    storage: process.env.UPLOADS_STORAGE || 'local',
+    profile: {
+      image: {
+        dest: './modules/users/client/img/profile/uploads/',
+        limits: {
+          fileSize: 1 * 1024 * 1024 // Max file size in bytes (1 MB)
+        }
       }
     }
   },
-  livereload: true,
-  seedDB: {
-    seed: process.env.MONGO_SEED === 'true',
-    options: {
-      logResults: process.env.MONGO_SEED_LOG_RESULTS !== 'false'
-    },
-    // Order of collections in configuration will determine order of seeding.
-    // i.e. given these settings, the User seeds will be complete before
-    // Article seed is performed.
-    collections: [{
-      model: 'User',
-      docs: [{
-        data: {
-          username: 'local-admin',
-          email: 'admin@localhost.com',
-          firstName: 'Admin',
-          lastName: 'Local',
-          roles: ['admin', 'user']
-        }
-      }, {
-        // Set to true to overwrite this document
-        // when it already exists in the collection.
-        // If set to false, or missing, the seed operation
-        // will skip this document to avoid overwriting it.
-        overwrite: true,
-        data: {
-          username: 'local-user',
-          email: 'user@localhost.com',
-          firstName: 'User',
-          lastName: 'Local',
-          roles: ['user']
-        }
-      }]
-    }, {
-      model: 'Article',
-      options: {
-        // Override log results setting at the
-        // collection level.
-        logResults: true
-      },
-      skip: {
-        // Skip collection when this query returns results.
-        // e.g. {}: Only seeds collection when it is empty.
-        when: {} // Mongoose qualified query
-      },
-      docs: [{
-        data: {
-          title: 'First Article',
-          content: 'This is a seeded Article for the development environment'
-        }
-      }]
-    }]
+  shared: {
+    owasp: {
+      allowPassphrases: true,
+      maxLength: 128,
+      minLength: 10,
+      minPhraseLength: 20,
+      minOptionalTestsToPass: 4
+    }
   }
+
 };
