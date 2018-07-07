cb449dea5a869b1f936bd6da76801483c4a08d1c
Akash Agarwal <akash.kumar@axisrooms.com>
2018-07-07 13:05:59 +0530

    text

--- modules/bookings/client/controllers/booking-edit.client.controller.js
+++ modules/bookings/client/controllers/booking-edit.client.controller.js
@@ -147,11 +147,23 @@
       }
     }
 
-    $('input[type="text"]').keyup(function(e) {
-        if(e.keyCode == 13) {
-            $(this).next().focus();
-        }
-    });
+        $('input:text').bind("keydown", function(e) {
+          var n = $("input:text").length;
+          if (e.which == 13) 
+          { //Enter key
+            e.preventDefault(); //to skip default behavior of the enter key
+            var nextIndex = $('input:text').index(this) + 1;
+            if(nextIndex < n)
+              $('input:text')[nextIndex].focus();
+            else
+            {
+              $('input:text')[nextIndex-1].blur();
+              $('#btnSubmit').click();
+            }
+          }
+        });
+
+
 
   }
 }());
