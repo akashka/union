7dc15f4c01fa172c76a5bc140b28db67e78a9f42
Akash Agarwal <30892454+akashka@users.noreply.github.com>
2020-07-16 12:33:34 +0530

    replace with bills

--- modules/bills/client/bills.client.module.js
+++ modules/bills/client/bills.client.module.js
@@ -1,4 +1,5 @@
 (function (app) {
+  'use strict';
 
   app.registerModule('bills', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
   app.registerModule('bills.admin', ['core.admin']);
